1502310e8057c41698d516cd9432bda2defc1ca6
Doshik <stedasha28@mail.ru>
2024-03-19 22:50:21 +0300

    end laba-7

--- lab-7.py
+++ lab-7.py
@@ -47,15 +47,42 @@ ax2 = fig.add_subplot(1, 2, 2)
 fig.subplots_adjust(wspace=0.5)
 
 ax1.hist(sulfate_values, bins = 20)
-ax1.set_title('Distribution')
-ax1.set_xlabel('Sulfate values')
-ax1.set_ylabel('Frequency')
+ax1.set_title('Распределение')
+ax1.set_xlabel('значение измерения')
+ax1.set_ylabel('частота')
 
 ax2.hist(sulfate_values, bins = 20, density=True)
-ax2.set_title('Normal distribution')
-ax2.set_xlabel('Sulfate values')
-ax2.set_ylabel('Frequency')
+ax2.set_title('Нормальное распределение')
+ax2.set_xlabel('значение измерения')
+ax2.set_ylabel('частота')
 plt.show()
+
+#Доп. задание
+LENGTH = 2 * np.pi
+
+animation = PillowWriter(60)
+fig = plt.figure()
+ax = fig.add_subplot()
+
+animation.setup(fig, 'sin(x).gif')
+
+ax.set_title('График функции y = sin(x)')
+ax.set_xlabel('x')
+ax.set_ylabel('sin(x)')
+
+xs = np.linspace(0, LENGTH, 100)
+ys = np.sin(xs)
+ax.plot(xs, ys)
+dot, = plt.plot([0], [np.sin(0)], 'ro')
+animation.grab_frame()
+
+slides = np.linspace(0, LENGTH, 60)
+for i in slides:
+    dot.set_data(i, np.sin(i))
+    animation.grab_frame()
+
+animation.finish()
+
 
 
 #Задание 3
